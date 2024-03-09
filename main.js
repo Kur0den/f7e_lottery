eabadfa286c8ad8364fab5abe39a1e8a504e6a34
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2024-03-10 01:24:44 +0900

    webfignerを確認するコードを追加

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-function runButton() {
-    console.log('The button was clicked');
-}
+$(function () {
+    $('#run-button').on('click', function () {
+        console.log('The button was clicked');
 
-function copyButton() {
-    console.log('The button was clicked');
-}
+        const instanceDomain = $('#instance-input').val();
+        const accountName = $('#account-input').val();
 
-window.onload = function () {
-    const runButton = document.getElementById('run-button');
-    const copyButton = document.getElementById('copy-button');
-
-    runButton.addEventListener('click', () => {
-        runButton();
+        instanceCheck(instanceDomain).then(function (data) {
+            console.log(data);
+            const webFingerData = data.replace('{uri}', 'acct:' + accountName + '@' + instanceDomain);
+            console.log(webFingerData);
+            accountCheck(webFingerData, accountName);
+        });
     });
 
-    copyButton.addEventListener('click', () => {
-        copyButton();
+    $('#copy-button').on('click', function () {
+        console.log('The button was clicked');
     });
-};
+});
+
+function instanceCheck(instanceDomain) {
+    console.log('instanceCheck');
+
+    var dfd = $.Deferred();
+
+    $.ajax({
+        url: 'https://' + instanceDomain + '/.well-known/host-meta.json',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.log('success');
+            console.log(data);
+            dfd.resolve(data['links'][0]['template']);
+        },
+        error: function (data) {
+            console.log('error');
+            console.log(data);
+            dfd.reject();
+        },
+    });
+
+    return dfd.promise();
+}
+
+// 入力されたインスタンス、アカウントが正しいかどうか確認
+function accountCheck(instanceDomain, accountName) {}
